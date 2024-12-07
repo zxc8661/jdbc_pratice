d04c6eb09d7b60afbcfaf2748739921193b43e49
janjan <97529177+zxc8661@users.noreply.github.com>
2024-12-07 14:59:30 +0900

    setting

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("java")
+}
+
+group = "com.ll"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    runtimeOnly("com.mysql:mysql-connector-j:9.1.0")
+
+    compileOnly("org.projectlombok:lombok:1.18.36")
+    annotationProcessor("org.projectlombok:lombok:1.18.36")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.2")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.18.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/main/java/com/ll/DatabaseController.java
@@ -0,0 +1,5 @@
+package com.ll;
+
+public class DatabaseController {
+
+}
